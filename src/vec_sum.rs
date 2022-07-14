38d58e6b25d3415ab1407b634aae9106a97f1f5d
craftpan <craft.pan@outlook.com>
2022-07-14 23:45:54 +0800

    format code

--- src/vec_sum.rs
+++ src/vec_sum.rs
@@ -1,18 +1,18 @@
 use rand::Rng;
 
-pub fn print(){
-    let mut v:Vec<u32> =Vec::new(); // u32的vec集合
-    let mut random = rand::thread_rng(); 
-    for _i in 0..11{
+pub fn print() {
+    let mut v: Vec<u32> = Vec::new(); // u32的vec集合
+    let mut random = rand::thread_rng();
+    for _i in 0..11 {
         v.push(random.gen_range(40..100)); //生成随机数
     }
-    println!("Vec: {:?}",v);
-    println!("{:?}",sum(&v));
+    println!("Vec: {:?}", v);
+    println!("{:?}", sum(&v));
 }
 
-fn sum(item:&Vec<u32>) -> Option<u32>{
-    let mut total:u32= 0;
-    for i in item.into_iter(){
+fn sum(item: &Vec<u32>) -> Option<u32> {
+    let mut total: u32 = 0;
+    for i in item.into_iter() {
         total += i;
     }
     Some(total)
